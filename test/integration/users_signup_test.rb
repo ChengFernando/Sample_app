7ecf7aa2b4e55f5280f9e050500f9164555631c0
ChengFernando <fernandocheng2010@gmail.com>
2016-03-09 11:01:34 +0800

    Add password resets & email configuration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     ActionMailer::Base.deliveries.clear
   end
@@ -15,8 +15,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -24,7 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                email: "user@example.com",
                                password:              "password",
                                password_confirmation: "password" }
-      end
+    end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
@@ -34,7 +36,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # Invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
-    # valid token, wrong email
+    # Valid token, wrong email
     get edit_account_activation_path(user.activation_token, email: 'wrong')
     assert_not is_logged_in?
     # Valid activation token
@@ -45,5 +47,3 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
   end
 end
-
-
